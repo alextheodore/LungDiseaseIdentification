5194841158813946df1747d35aeb96d89098c165
alextheodore <alex.theodore26@gmail.com>
2024-03-26 18:02:41 +0700

    update_home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,13 +1,19 @@
-import React from 'react'    
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from "../../asset/image/icon.png";
 import "./index.css";
-import { useState } from 'react';   
+import { useState } from "react";
 
 function HomeComponent() {
+  const navigate = useNavigate();
+
   return (
-    <div>home</div>
-  )
+    <>
+      <div className="container">
+        <h1 style={{ fontWeight: "bold", fontSize: "40px" }}>Home Page</h1>
+      </div>
+    </>
+  );
 }
 
-export default HomeComponent
+export default HomeComponent;
